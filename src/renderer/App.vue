cf5a8395f60e83d6fe203a0eabaf5a5b3e25448a
Meet Godhani <meetgodhani23@gmail.com>
2018-03-21 23:30:24 -0400

    Base layout

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,111 +1,103 @@
 <template>
   <div id="app">
-    <v-app dark>
+    <v-app>
       <v-navigation-drawer
-        fixed
-        :mini-variant="miniVariant"
-        :clipped="clipped"
-        v-model="drawer"
-        app
+      fixed
+      :clipped="$vuetify.breakpoint.lgAndUp"
+      v-model="drawer"
+      app
       >
-        <v-list>
-          <v-list-tile
-            router
-            :to="item.to"
-            :key="i"
-            v-for="(item, i) in items"
-            exact
-          >
-            <v-list-tile-action>
-              <v-icon v-html="item.icon"></v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title v-text="item.title"></v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </v-list>
-      </v-navigation-drawer>
-      <v-toolbar fixed app :clipped-left="clipped">
-        <v-toolbar-side-icon @click.native.stop="drawer = !drawer"></v-toolbar-side-icon>
-        <v-btn
-          icon
-          @click.native.stop="miniVariant = !miniVariant"
+      <v-list>
+        <v-list-tile
+        router
+        :to="item.to"
+        :key="i"
+        v-for="(item, i) in items"
+        exact
         >
-          <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-        </v-btn>
-        <v-btn
-          icon
-          @click.native.stop="clipped = !clipped"
-        >
-          <v-icon>web</v-icon>
-        </v-btn>
-        <v-btn
-          icon
-          @click.native.stop="fixed = !fixed"
-        >
-          <v-icon>remove</v-icon>
-        </v-btn>
-        <v-toolbar-title v-text="title"></v-toolbar-title>
-        <v-spacer></v-spacer>
-        <v-btn
-          icon
-          @click.native.stop="rightDrawer = !rightDrawer"
-        >
-          <v-icon>menu</v-icon>
-        </v-btn>
-      </v-toolbar>
-      <v-content>
-        <v-container fluid fill-height>
-          <v-slide-y-transition mode="out-in">
-            <router-view></router-view>
-          </v-slide-y-transition>
-        </v-container>
-      </v-content>
-      <v-navigation-drawer
-        temporary
-        fixed
-        :right="right"
-        v-model="rightDrawer"
-        app
-      >
-        <v-list>
-          <v-list-tile @click.native="right = !right">
-            <v-list-tile-action>
-              <v-icon light>compare_arrows</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-          </v-list-tile>
-        </v-list>
-      </v-navigation-drawer>
-      <v-footer :fixed="fixed" app>
-        <v-spacer></v-spacer>
-        <span>&copy; 2017</span>
-      </v-footer>
-    </v-app>
-  </div>
+        <v-list-tile-action>
+          <v-icon v-html="item.icon"></v-icon>
+        </v-list-tile-action>
+        <v-list-tile-content>
+          <v-list-tile-title v-text="item.title"></v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+    <v-layout align-center>
+      <v-flex xs6>
+        <v-subheader>
+          Categories
+        </v-subheader>
+      </v-flex>
+      <v-flex xs6 class="text-xs-right">
+        <v-btn small flat>edit</v-btn>
+      </v-flex>
+    </v-layout>
+    <v-layout align-center>
+      <v-flex xs6>
+        <v-subheader>
+          Subscriptions
+        </v-subheader>
+      </v-flex>
+      <v-flex xs6 class="text-xs-right">
+        <v-btn small flat>edit</v-btn>
+      </v-flex>
+    </v-layout>
+  </v-navigation-drawer>
+  <v-toolbar fixed app dark color="primary" :clipped-left="$vuetify.breakpoint.lgAndUp">
+    <v-toolbar-side-icon @click.native.stop="drawer = !drawer"></v-toolbar-side-icon>
+    <v-toolbar-title class="ml-0 pl-3 pr-3">
+      <span class="hidden-sm-and-down">{{ title }}</span>
+    </v-toolbar-title>
+    <v-toolbar-items>
+      <v-btn flat>Import</v-btn>
+      <v-btn flat>Export</v-btn>
+      <v-btn icon>
+        <v-icon>refresh</v-icon>
+      </v-btn>
+    </v-toolbar-items>
+    <v-spacer></v-spacer>
+    <v-text-field
+    flat
+    solo-inverted
+    prepend-icon="search"
+    label="Search"
+    class="hidden-sm-and-down"
+    ></v-text-field>
+  </v-toolbar>
+  <v-content>
+    <v-container fluid fill-height>
+      <v-slide-y-transition mode="out-in">
+        <router-view></router-view>
+      </v-slide-y-transition>
+    </v-container>
+  </v-content>
+</v-app>
+</div>
 </template>
 
 <script>
-  export default {
-    name: 'rss-reader',
-    data: () => ({
-      clipped: false,
-      drawer: true,
-      fixed: false,
-      items: [
-        { icon: 'apps', title: 'Welcome', to: '/' },
-        { icon: 'bubble_chart', title: 'Inspire', to: '/inspire' },
-        { icon: 'star_outline', title: 'Favourites', to: '/inspire' }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
-    })
-  }
+export default {
+  name: 'rss-reader',
+  data: () => ({
+    clipped: true,
+    drawer: true,
+    fixed: true,
+    items: [
+      { icon: 'apps', title: 'All Articles', to: '/' },
+      { icon: 'bubble_chart', title: 'Unread', to: '/inspire' },
+      { icon: 'star_outline', title: 'Favourites', to: '/inspire' }
+    ],
+    miniVariant: false,
+    right: true,
+    rightDrawer: false,
+    title: 'All Articles'
+  })
+}
 </script>
 
 <style>
-  @import url('https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons');
-  /* Global CSS */
+@import url('https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons');
+@import url('https://fonts.googleapis.com/css?family=Playfair+Display:400,700');
+/* Global CSS */
 </style>
--- src/renderer/main.js
+++ src/renderer/main.js
@@ -7,7 +7,17 @@ import App from './App'
 import router from './router'
 import store from './store'
 
-Vue.use(Vuetify)
+Vue.use(Vuetify, {
+  theme: {
+    primary: '#283593',
+    secondary: '#1E88E5',
+    accent: '#9c27b0',
+    error: '#f44336',
+    warning: '#ffeb3b',
+    info: '#2196f3',
+    success: '#4caf50'
+  }
+})
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
