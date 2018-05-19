c2273d2f99c3a8981c39a1a1399df64e7db2e315
Meet Godhani <meetgodhani23@gmail.com>
2018-05-19 12:02:16 -0400

    First clean commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'rss-reader'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
