c25e2da848013290f7f940c743da9d23fd03e2b0
Meet Godhani <meetgodhani23@gmail.com>
2018-06-10 13:11:11 -0400

    Added mercury parsing service

--- src/renderer/parsers/article.js
+++ src/renderer/parsers/article.js
@@ -1,55 +1,17 @@
-// import dayjs from 'dayjs'
-// import normalize from 'normalize-url'
-import FeedParser from 'feedparser'
-import zlib from 'zlib'
 import axios from 'axios'
+import { MERCURY_API_TOKEN } from '../config'
 
-const useragent = 'RSS Reader'
-const acceptheader = 'text/html, application/xhtml+xml, application/xml'
+const apiUri = 'https://mercury.postlight.com/parser'
 
-export async function parseFeed (feedUrl) {
-  const stream = await ReadFeed(feedUrl)
-  const posts = await ReadFeedStream(stream)
-  // const response = await ParseFeedPost(posts)
-  // return response
-  return posts
-}
-
-export async function ReadFeed (url) {
-  const headers = {
-    'User-Agent': useragent,
-    'Accept': acceptheader,
-    'Accept-Encoding': 'gzip, deflate'
-  }
-  const response = await axios({
-    method: 'get',
-    url: url,
-    responseType: 'stream',
-    timeout: 7000,
-    headers: headers,
-    maxRedirects: 20
+export async function parseArticle (url) {
+  const result = await axios.get(apiUri, {
+    headers: {
+      'Content-Type': 'application/json',
+      'x-api-key': MERCURY_API_TOKEN
+    },
+    params: {
+      url: url
+    }
   })
-  const encoding = response.headers['content-encoding']
-  if (encoding === 'gzip' || encoding === 'deflate') {
-    response.data.pipe(zlib.createGunzip())
-  }
-  return response
-}
-
-export async function ReadFeedStream (stream) {
-  const posts = []
-  return new Promise((resolve, reject) => {
-    stream.pipe(new FeedParser())
-      .on('error', reject)
-      .on('end', () => {
-        resolve(posts)
-      })
-      .on('readable', () => {
-        const stream = this
-        let item
-        while ((item = stream.read())) {
-          posts.push(item)
-        }
-      })
-  })
+  return result
 }
