8fea15305385bc368dbd567b3051483a61c6b205
Meet Godhani <meetgodhani23@gmail.com>
2018-06-25 22:18:47 -0400

    Added new updater

--- src/main/index.js
+++ src/main/index.js
@@ -1,9 +1,16 @@
 'use strict'
 
 import { app, BrowserWindow, Menu } from 'electron'
-import { autoUpdater } from 'electron-updater'
+import updateElectron from 'update-electron-app'
+import electronLog from 'electron-log'
 
 let myWindow = null
+
+updateElectron({
+  repo: 'mrgodhani/rss-reader',
+  updateInterval: '1 hour',
+  logger: electronLog
+})
 
 /**
 * Set `__static` path to static files in production
@@ -87,20 +94,4 @@ app.on('activate', () => {
   if (mainWindow === null) {
     createWindow()
   }
-})
-
-/**
-  * Auto Updater
-  *
-  * Uncomment the following code below and install `electron-updater` to
-  * support auto updating. Code Signing with a valid certificate is required.
-  * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-electron-builder.html#auto-updating
-  */
-
-autoUpdater.on('update-downloaded', () => {
-  autoUpdater.quitAndInstall()
-})
-
-app.on('ready', () => {
-  if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
 })
